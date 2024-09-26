dd905080951acfad75b9204d2e3ad8eed9d69adb
Manrique31 <144976323+Manrique31@users.noreply.github.com>
2024-09-25 20:05:21 -0600

    Creación de proyecto
    Se creo proyecto desde cero

---
+++ TiendaManriqueTorres/src/main/java/Tienda/demo/TiendaManriqueTorresApplication.java
@@ -0,0 +1,13 @@
+package Tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaManriqueTorresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaManriqueTorresApplication.class, args);
+	}
+
+}
