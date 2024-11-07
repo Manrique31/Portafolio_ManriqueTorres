af93ce683a1cf87d4cbbefa8d395a3eddd912bf8
Manrique31 <144976323+Manrique31@users.noreply.github.com>
2024-11-06 22:44:12 -0600

    Semana 7
    Se agrega el avance del portafolio sin errores

--- TiendaManriqueTorres/src/main/java/TiendaManriqueTorres/demo/domain/Categoria.java
+++ TiendaManriqueTorres/src/main/java/TiendaManriqueTorres/demo/domain/Categoria.java
@@ -2,6 +2,7 @@ package TiendaManriqueTorres.demo.domain;
 
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import lombok.Data;
 
 @Data
@@ -19,6 +20,9 @@ public class Categoria implements Serializable {
     private String rutaImagen;
     private boolean activo;
 
+     @OneToMany
+    @JoinColumn(name="id_categoria")
+    List<Producto> Productos;
     public Categoria() {
     }
 
